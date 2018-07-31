38d417fa9dd09647a5d1791f289c55eb1336a640
Neil Hiddink <nhiddink2@gmail.com>
2018-07-31 18:02:51 -0400

    feat: (StormViewer) Work on UITableViewController Code

--- StormViewer/StormViewer/Controllers/ViewController.swift
+++ StormViewer/StormViewer/Controllers/ViewController.swift
@@ -8,11 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// MARK: ViewController: UIViewController
 
+class ViewController: UITableViewController {
+
+    // MARK: Properties
+    
+    var pictures = [String]()
+    
+    // MARK: IB Outlets
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    // MARK: View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let fileManager = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fileManager.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
+        
+    }
+    
+}
+
+// MARK: UITableViewDelegate, UITableViewDataSource
+
+extension ViewController {
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "ImageCell", for: indexPath) {
+            cell.imageView?.image = UIImage(contentsOfFile: pictures[indexPath.row])
+            return cell
+        }
+        return UITableViewCell()
     }
     
 }
