389f0e9e5714d29be370fb2349f7a04dee208ff7
Neil Hiddink <nhiddink2@gmail.com>
2018-07-31 13:55:11 -0400

    feat: (StormViewer) Organize Project Files and Add Image Content to Asset Catalog

---
+++ StormViewer/StormViewer/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by Neil Hiddink on 7/31/18.
+//  Copyright © 2018 Neil Hiddink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
+
