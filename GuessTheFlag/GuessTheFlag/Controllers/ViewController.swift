29d52c5c5f4744f828f5414b8cab9eda9eb1945c
Neil Hiddink <nhiddink2@gmail.com>
2018-08-01 13:47:10 -0400

    feat: (GuessTheFlag) Complete Guess the Flag

--- GuessTheFlag/GuessTheFlag/Controllers/ViewController.swift
+++ GuessTheFlag/GuessTheFlag/Controllers/ViewController.swift
@@ -17,11 +17,13 @@ class ViewController: UIViewController {
     var countries = [String]()
     var buttons = [UIButton]()
     
-    var countryString: String?
+    var countryString = ""
     
     var topCountry: String?
     var middleCountry: String?
     var bottomCountry: String?
+    
+    var score = 0
     
     // MARK: IB Outlets
     
@@ -37,75 +39,127 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupGame()
-    }
-    
-    // MARK: Private Methods
-    
-    private func setupGame() {
         countries = ["estonia", "france", "germany", "ireland", "italy", "monaco",
                      "nigeria", "poland", "russia", "spain", "uk", "us"]
-        questionLabel.text! = askQuestion()
         
         buttons = [topButton, middleButton, bottomButton]
         for button in buttons {
             button.layer.borderWidth = 1
             button.layer.borderColor = UIColor.lightGray.cgColor
         }
+        
+        askQuestion()
     }
     
-    private func askQuestion() -> String {
+    // MARK: Private Methods
+    
+    private func resetGame(action: UIAlertAction! = nil) {
         
-        let random1 = Int(arc4random_uniform(12))
-        let random2 = Int(arc4random_uniform(12))
-        let random3 = Int(arc4random_uniform(12))
+        askQuestion()
         
-        guard random1 != random2, random1 != random3, random2 != random3 else { return ""}
+        topStatusLabel.text = nil
+        middleStatusLabel.text = nil
+        bottomStatusLabel.text = nil
+    }
+    
+    private func askQuestion() {
         
-        topButton.setImage(UIImage(named: countries[random1]), for: .normal)
-        middleButton.setImage(UIImage(named: countries[random2]), for: .normal)
-        bottomButton.setImage(UIImage(named: countries[random3]), for: .normal)
+        var threeUniqueValues = false
+        var random1 = 0
+        var random2 = 0
+        var random3 = 0
+        while threeUniqueValues == false {
         
-        let randomQ = Int(arc4random_uniform(3))
-        
-        switch randomQ {
-            case 1:
-                countryString = countries[random1]
-                topCountry = countryString
-            case 2:
-                countryString = countries[random2]
-                middleCountry = countryString
-            case 3:
-                countryString = countries[random3]
-                bottomCountry = countryString
-            default:
+            random1 = Int(arc4random_uniform(UInt32(countries.count)))
+            random2 = Int(arc4random_uniform(UInt32(countries.count)))
+            random3 = Int(arc4random_uniform(UInt32(countries.count)))
+            
+            if random1 != random2, random1 != random3, random2 != random3 {
+                threeUniqueValues = true
+                
+                let randomQ = Int(arc4random_uniform(3))
+                switch randomQ {
+                    case 1:
+                        countryString = countries[random1]
+                    case 2:
+                        countryString = countries[random2]
+                    case 3:
+                        countryString = countries[random3]
+                    default:
+                        break
+                }
+                
+                topButton.setImage(UIImage(named: countries[random1]), for: .normal)
+                topCountry = countries[random1]
+                middleButton.setImage(UIImage(named: countries[random2]), for: .normal)
+                middleCountry = countries[random2]
+                bottomButton.setImage(UIImage(named: countries[random3]), for: .normal)
+                bottomCountry = countries[random3]
+                
+                questionLabel.text = "Please select the flag for:\n\((countryString.uppercased()))"
+                
                 break
+            }
+            
         }
         
-        return "Please select the flag for:\n\(countryString!.uppercased())"
+    }
+    
+    private func updateScore(correct: Bool) {
+        var titleString = ""
+        if correct {
+            score += 1
+            titleString = "Correct"
+        } else {
+            score -= 1
+            titleString = "Incorrect"
+        }
+        
+        let alert = UIAlertController(title: titleString, message: "Score: \(score)", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Close", style: .default) { _ in
+            self.resetGame()
+        })
+        present(alert, animated: true)
     }
 
     // MARK: IB Actions
     
     @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        topStatusLabel.text = nil
+        middleStatusLabel.text = nil
+        bottomStatusLabel.text = nil
+        
         switch sender {
             case topButton:
-                if countryString == self.topCountry! {
+                if countryString.uppercased() == self.topCountry?.uppercased() {
                     topStatusLabel.text = "✓"
+                    topStatusLabel.textColor = UIColor.green
+                    updateScore(correct: true)
                 } else {
                     topStatusLabel.text = "✗"
+                    topStatusLabel.textColor = UIColor.red
+                    updateScore(correct: false)
                 }
             case middleButton:
-                if countryString == self.middleCountry! {
+                if countryString.uppercased() == self.middleCountry?.uppercased() {
                     middleStatusLabel.text = "✓"
+                    topStatusLabel.textColor = UIColor.green
+                    updateScore(correct: true)
                 } else {
                     middleStatusLabel.text = "✗"
+                    topStatusLabel.textColor = UIColor.red
+                    updateScore(correct: false)
                 }
             case bottomButton:
-                if countryString == self.bottomCountry! {
+                if countryString.uppercased() == self.bottomCountry?.uppercased() {
                     bottomStatusLabel.text = "✓"
+                    topStatusLabel.textColor = UIColor.green
+                    updateScore(correct: true)
                 } else {
                     bottomStatusLabel.text = "✗"
+                    topStatusLabel.textColor = UIColor.red
+                    updateScore(correct: false)
                 }
             default:
                 break
