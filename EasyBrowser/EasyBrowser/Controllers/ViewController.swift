b7c6bd4ddda7bcd10a86b2e8cbc44a94d447341c
Neil Hiddink <nhiddink2@gmail.com>
2018-08-02 11:33:16 -0400

    feat: (EasyBrowser) Implement UIBarButtonItem - Open for Several URLs

--- EasyBrowser/EasyBrowser/Controllers/ViewController.swift
+++ EasyBrowser/EasyBrowser/Controllers/ViewController.swift
@@ -31,9 +31,36 @@ class ViewController: UIViewController {
         let url = URL(string: "https://www.apple.com")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
     }
 
+    
+    // MARK: Helper Methods
+    
+    @objc func openTapped() {
+        let alert = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        alert.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alert.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        present(alert, animated: true)
+    }
+    
+    @objc func openPage(action: UIAlertAction) {
+        if let url = URL(string: "https://" + action.title!) {
+            webView.load(URLRequest(url: url))
+        }
+    }
 }
 
-extension ViewController: WKNavigationDelegate {}
+// MARK: ViewController: WKNavigationDelegate
+
+extension ViewController: WKNavigationDelegate {
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+    
+}
 
