25a8333494c2cf8450b53e975e2d0dc97b3a815c
Neil Hiddink <nhiddink2@gmail.com>
2018-08-03 22:36:11 -0400

    feat: (WHPetitions) Add+Organize Project Files, Setup Initial UI

---
+++ 7. WHPetitions/WHPetitions/Controllers/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  WHPetitions
+//
+//  Created by Neil Hiddink on 8/3/18.
+//  Copyright © 2018 Neil Hiddink. All rights reserved.
+//
+
+import UIKit
+
+// MARK: ViewController: UIViewController
+
+class ViewController: UIViewController {
+
+    // MARK: Properties
+    
+    var petitions = [String]()
+    
+    // MARK: IB Outlets
+    
+    @IBOutlet var tableView: UITableView!
+    
+    // MARK: View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
+
+// MARK: ViewController: UITableViewDelegate, UITableViewDataSource
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = ""
+        cell.detailTextLabel?.text = ""
+        return cell
+    }
+}
