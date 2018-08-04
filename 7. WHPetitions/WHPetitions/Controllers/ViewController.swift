aae19135e8f309ce4e1bf359ca1084dc366141be
Neil Hiddink <nhiddink2@gmail.com>
2018-08-04 10:59:19 -0400

    feat: (WHPetitions) Add DetailViewController with Body Text

--- 7. WHPetitions/WHPetitions/Controllers/ViewController.swift
+++ 7. WHPetitions/WHPetitions/Controllers/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
 
     // MARK: Properties
     
-    var petitions = [String]()
+    var petitions = [[String: String]]()
     
     // MARK: IB Outlets
     
@@ -25,8 +25,33 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        getPetitionsFor(url: "https://api.whitehouse.gov/v1/petitions.json?limit=100")
     }
     
+    // MARK: Helper Methods
+    
+    func getPetitionsFor(url: String) {
+        if let url = URL(string: url) {
+            if let data = try? String(contentsOf: url) {
+                let json = JSON(parseJSON: data)
+                if json["metadata"]["responseInfo"]["status"].intValue == 200 {
+                    parse(json: json)
+                }
+            }
+        }
+    }
+    
+    func parse(json: JSON) {
+        for result in json["results"].arrayValue {
+            let title = result["title"].stringValue
+            let body = result["body"].stringValue
+            let sigs = result["signatureCount"].stringValue
+            let obj = ["title": title, "body": body, "sigs": sigs]
+            petitions.append(obj)
+        }
+        
+        tableView.reloadData()
+    }
 }
 
 // MARK: ViewController: UITableViewDelegate, UITableViewDataSource
@@ -34,7 +59,9 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        let detailVC = DetailViewController()
+        detailVC.detailPetition = petitions[indexPath.row]
+        navigationController?.pushViewController(detailVC, animated: true)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -43,8 +70,9 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = ""
-        cell.detailTextLabel?.text = ""
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition["title"]
+        cell.detailTextLabel?.text = petition["body"]
         return cell
     }
 }
