21d9c07096a9414d565ef2de760e8573ddfcf3a6
Neil Hiddink <nhiddink2@gmail.com>
2018-08-05 17:29:00 -0400

    feat: (SevenSwiftyWords) Organize Project Files

---
+++ 8. SevenSwiftyWords/SevenSwiftyWords/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SevenSwiftyWords
+//
+//  Created by Neil Hiddink on 8/5/18.
+//  Copyright © 2018 Neil Hiddink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
