59a38ad56e061bfe76b6f7196e550e8edd75c488
Neil Hiddink <nhiddink2@gmail.com>
2018-08-06 12:46:23 -0400

    feat: (SevenSwiftyWords) Implement LoadLevel Method

--- 8. SevenSwiftyWords/SevenSwiftyWords/Controllers/ViewController.swift
+++ 8. SevenSwiftyWords/SevenSwiftyWords/Controllers/ViewController.swift
@@ -7,11 +7,20 @@
 //
 
 import UIKit
+import GameplayKit
 
 // MARK: ViewController: UIViewController
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    
+    var letterButtons = [UIButton]()
+    var activatedButtons = [UIButton]()
+    var solutions = [String]()
+    var score = 0
+    var level = 1
+    
     // MARK: IB Outlets
     
     @IBOutlet weak var cluesLabel: UILabel!
@@ -24,6 +33,52 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        for subview in view.subviews where subview.tag == 1001 {
+            let button = subview as! UIButton
+            letterButtons.append(button)
+            button.addTarget(self, action: #selector(letterTapped), for: .touchUpInside)
+        }
+        
+        loadLevel()
+    }
+    
+    // MARK: Helper Methods
+    
+    @objc func letterTapped(button: UIButton) {
+        
+    }
+    
+    func loadLevel() {
+        var clueString = ""
+        var solutionString = ""
+        var letterBits = [String]()
+        
+        if let levelFilePath = Bundle.main.path(forResource: "level\(level)", ofType: "txt") {
+            if let levelContents = try? String(contentsOfFile: levelFilePath) {
+                var lines = levelContents.components(separatedBy: "\n")
+                lines = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: lines) as! [String]
+                for (index, line) in lines.enumerated() {
+                    let parts = line.components(separatedBy: ": ")
+                    let answer = parts[0]
+                    let clue = parts[1]
+                    clueString += "\(index + 1). \(clue)\n"
+                    let solutionWord = answer.replacingOccurrences(of: "|", with: "")
+                    solutionString += "\(solutionWord.count) letters\n"
+                    solutions.append(solutionWord)
+                    let bits = answer.components(separatedBy: "|")
+                    letterBits += bits
+                }
+            }
+        }
+        
+        cluesLabel.text = clueString.trimmingCharacters(in: .whitespacesAndNewlines)
+        answersLabel.text = solutionString.trimmingCharacters(in: .whitespacesAndNewlines)
+        letterBits = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: letterBits) as! [String]
+        if letterBits.count == letterButtons.count {
+            for i in 0 ..< letterBits.count {
+                letterButtons[i].setTitle(letterBits[i], for: .normal)
+            }
+        }
     }
 
     // MARK: IB Actions
