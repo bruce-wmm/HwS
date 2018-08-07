4c98fe15512c05f869d5d8b294e55f76865cb0fa
Neil Hiddink <nhiddink2@gmail.com>
2018-08-07 14:05:42 -0400

    feat: (Hangman) Organize Project Files

---
+++ 9b. Hangman/Hangman/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hangman
+//
+//  Created by Neil Hiddink on 8/7/18.
+//  Copyright © 2018 Neil Hiddink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
