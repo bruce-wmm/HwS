6953965ffa1fe271c402b59b78d940ea646e9232
Neil Hiddink <nhiddink2@gmail.com>
2018-08-14 21:30:56 -0400

    feat: (NamesToFaces) Implement UIImagePickerController in ViewController.swift

--- 10. NamesToFaces/NamesToFaces/Controllers/ViewController.swift
+++ 10. NamesToFaces/NamesToFaces/Controllers/ViewController.swift
@@ -8,21 +8,41 @@
 
 import UIKit
 
+// MARK: ViewController: UICollectionViewController
+
 class ViewController: UICollectionViewController {
 
+    // MARK: View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
+        
     }
 
-
-}
-
-extension ViewController {
+    // MARK: Helper Methods
+    
+    @objc func addNewPerson() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+    
+    // MARK: UICollectionView Delegate Methods
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
     }
+    
+    // MARK: UICollectionView Data Source Methods
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 10
@@ -31,6 +51,24 @@ extension ViewController {
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as! PersonCell
         return cell
+    }
+
+}
+
+// MARK: ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else { return }
+        
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+        
+        if let jpegData = image.jpegData(compressionQuality: 80) {
+            try? jpegData.write(to: imagePath)
+        }
+        dismiss(animated: true)
     }
     
 }
