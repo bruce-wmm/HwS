e935d5284998f8b633ae7c2d36c63a8da1574510
Neil Hiddink <nhiddink2@gmail.com>
2018-08-19 05:04:55 -0400

    feat: (Peggle) Implement SKPhysicsContactDelegate

--- 11. Peggle/Peggle/Controllers/GameScene.swift
+++ 11. Peggle/Peggle/Controllers/GameScene.swift
@@ -22,6 +22,7 @@ class GameScene: SKScene {
         addChild(background)
         
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        physicsWorld.contactDelegate = self
         
         makeBouncer(at: CGPoint(x: 0, y: 0))
         makeBouncer(at: CGPoint(x: 256, y: 0))
@@ -52,20 +53,35 @@ class GameScene: SKScene {
         if isGood {
             slotBase = SKSpriteNode(imageNamed: "slotBaseGood")
             slotGlow = SKSpriteNode(imageNamed: "slotGlowGood")
+            slotBase.name = "good"
         } else {
             slotBase = SKSpriteNode(imageNamed: "slotBaseBad")
             slotGlow = SKSpriteNode(imageNamed: "slotGlowBad")
+            slotBase.name = "bad"
         }
         slotBase.position = position
         slotGlow.position = position
+        
+        slotBase.physicsBody = SKPhysicsBody(rectangleOf: slotBase.size)
+        slotBase.physicsBody?.isDynamic = false
+        
         addChild(slotBase)
         addChild(slotGlow)
-        
-        // SKActions
         
         let spin = SKAction.rotate(byAngle: .pi, duration: 10)
         let spinForever = SKAction.repeatForever(spin)
         slotGlow.run(spinForever)
+    }
+    
+    func collisionBetween(ball: SKNode, object: SKNode) {
+        if object.name == "good" {
+            destroy(ball: ball)
+        } else if object.name == "bad" {
+            destroy(ball: ball)
+        }
+    }
+    func destroy(ball: SKNode) {
+        ball.removeFromParent()
     }
     
     // MARK: Touch Methods
@@ -74,11 +90,29 @@ class GameScene: SKScene {
         if let touch = touches.first {
             let location = touch.location(in: self)
             let ball = SKSpriteNode(imageNamed: "ballRed")
+            ball.name = "ball"
             ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
             ball.physicsBody?.restitution = 0.4
+            ball.physicsBody!.contactTestBitMask = ball.physicsBody!.collisionBitMask
             ball.position = location
             addChild(ball)
         }
     }
     
+}
+
+// MARK: GameScene: SKPhysicsContactDelegate
+
+extension GameScene: SKPhysicsContactDelegate {
+    func didBegin(_ contact: SKPhysicsContact) {
+        
+        guard let nodeA = contact.bodyA.node else { return }
+        guard let nodeB = contact.bodyB.node else { return }
+        
+        if nodeA.name == "ball" {
+            collisionBetween(ball: nodeA, object: nodeB)
+        } else if nodeB.name == "ball" {
+            collisionBetween(ball: nodeB, object: nodeA)
+        }
+    }
 }
