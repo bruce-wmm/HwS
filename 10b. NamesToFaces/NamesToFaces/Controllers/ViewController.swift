9d980088ee3936c56c01d012ccf8c573817d641f
Neil Hiddink <nhiddink2@gmail.com>
2018-08-21 21:35:48 -0400

    feat: (Names To Faces) Create Separate Folders for Project 12 12a - NSCoding, 12b - Codable

---
+++ 10b. NamesToFaces/NamesToFaces/Controllers/ViewController.swift
@@ -0,0 +1,106 @@
+//
+//  ViewController.swift
+//  NamesToFaces
+//
+//  Created by Neil Hiddink on 8/8/18.
+//  Copyright © 2018 Neil Hiddink. All rights reserved.
+//
+
+import UIKit
+
+// MARK: ViewController: UICollectionViewController
+
+class ViewController: UICollectionViewController {
+
+    // MARK: Properties
+    
+    var people = [Person]()
+    
+    // MARK: View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
+        
+    }
+
+    // MARK: Helper Methods
+    
+    @objc func addNewPerson() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+    
+    // MARK: UICollectionView Delegate Methods
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let person = people[indexPath.item]
+        
+        let alert = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
+        alert.addTextField()
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alert.addAction(UIAlertAction(title: "OK", style: .default) { [unowned self, alert] _ in
+            let newName = alert.textFields![0]
+            person.name = newName.text!
+            self.collectionView?.reloadData()
+        })
+        present(alert, animated: true)
+    }
+    
+    // MARK: UICollectionView Data Source Methods
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as! PersonCell
+        
+        let person = people[indexPath.item]
+        cell.name.text = person.name
+        
+        let path = getDocumentsDirectory().appendingPathComponent(person.image)
+        cell.imageView.image = UIImage(contentsOfFile: path.path)
+        cell.imageView.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3).cgColor
+        cell.imageView.layer.borderWidth = 2
+        cell.imageView.layer.cornerRadius = 3
+        cell.layer.cornerRadius = 7
+        
+        return cell
+    }
+
+}
+
+// MARK: ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else { return }
+        
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+        
+        if let jpegData = image.jpegData(compressionQuality: 80) {
+            try? jpegData.write(to: imagePath)
+        }
+        
+        let person = Person(name: "Unknown", image: imageName)
+        people.append(person)
+        collectionView?.reloadData()
+        
+        dismiss(animated: true)
+    }
+    
+}
+
---
+++ 10b. NamesToFaces/NamesToFaces/Models/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by Neil Hiddink on 8/12/18.
+//  Copyright © 2018 Neil Hiddink. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
