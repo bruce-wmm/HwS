24a05cd29ae9c80262026dc816e0ce0f0d489fff
Neil Hiddink <nhiddink2@gmail.com>
2018-08-27 16:10:00 -0400

    feat: (Animation) Organize Project Files

---
+++ 15. Animation/Animation/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animation
+//
+//  Created by Neil Hiddink on 8/27/18.
+//  Copyright © 2018 Neil Hiddink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
