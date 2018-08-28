ad26cbad1f148daaebad99667e9c5247b343baa9
Neil Hiddink <nhiddink2@gmail.com>
2018-08-28 19:57:29 -0400

    feat: (Animation) Add Penguin as UIImageView and Implement Button - Tap

--- 15. Animation/Animation/Controllers/ViewController.swift
+++ 15. Animation/Animation/Controllers/ViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
+// MARK: ViewController: UIViewController
+
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    
+    var imageView: UIImageView!
+    var currentAnimation = 0
+    
+    // MARK: IB Outlets
+    
+    @IBOutlet var tap: UIButton!
+    
+    // MARK: View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 512, y: 384)
+        view.addSubview(imageView)
     }
     
-
+    // MARK: IB Actions
+    
+    @IBAction func tapped(_ sender: UIButton) {
+        currentAnimation += 1
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
+    }
+    
 }
 
