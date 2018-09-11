d25f4e7b9a720152205b162daec953b68b05971d
Neil Hiddink <nhiddink2@gmail.com>
2018-09-10 23:56:39 -0400

    feat: (JavaScript Injection) Add Bar Button Item and Implement Done Button

--- 16. JavaScriptInjection/Extension/ActionViewController.swift
+++ 16. JavaScriptInjection/Extension/ActionViewController.swift
@@ -9,29 +9,52 @@
 import UIKit
 import MobileCoreServices
 
+// MARK: ActionViewController: UIViewController
+
 class ActionViewController: UIViewController {
 
-    @IBOutlet weak var imageView: UIImageView!
-
+    // MARK: Properties
+    
+    var pageTitle = ""
+    var pageURL = ""
+    
+    // MARK: IB Outlets
+    
+    @IBOutlet weak var script: UITextView!
+    
+    // MARK: View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        
         if let inputItem = extensionContext!.inputItems.first as? NSExtensionItem {
             if let itemProvider = inputItem.attachments?.first as? NSItemProvider {
                 itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [unowned self] (dict, error) in
                     let itemDictionary = dict as! NSDictionary
                     let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as! NSDictionary
-                    print(javaScriptValues)
+                    // print(javaScriptValues)
+                    self.pageTitle = javaScriptValues["title"] as! String
+                    self.pageURL = javaScriptValues["URL"] as! String
+                    DispatchQueue.main.async {
+                        self.title = self.pageTitle
+                    }
                 }
             }
         }
 
     }
+    
+    // MARK: IB Actions
 
     @IBAction func done() {
-        // Return any edited content to the host app.
-        // This template doesn't do anything, so we just echo the passed in items.
-        self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
+        let item = NSExtensionItem()
+        let argument: NSDictionary = ["customJavaScript": script.text]
+        let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
+        let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
+        item.attachments = [customJavaScript]
+        extensionContext!.completeRequest(returningItems: [item])
     }
 
 }
