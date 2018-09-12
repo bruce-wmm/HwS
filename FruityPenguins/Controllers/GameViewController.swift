1d546711aa30d6ba09052112b4ce883d859855b3
Neil Hiddink <nhiddink2@gmail.com>
2018-09-12 18:19:35 -0400

    feat: (FruityPenguins) Rename and Organize Project Files

--- FruityPenguins/Controllers/GameViewController.swift
+++ FruityPenguins/Controllers/GameViewController.swift
@@ -1,6 +1,6 @@
 //
 //  GameViewController.swift
-//  FlappyPenguin
+//  FruityPenguins
 //
 //  Created by Neil Hiddink on 9/6/18.
 //  Copyright © 2018 Neil Hiddink. All rights reserved.
@@ -12,39 +12,22 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
+    override var shouldAutorotate: Bool { return true }
+    override var prefersStatusBarHidden: Bool { return true }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
-                
-                // Present the scene
                 view.presentScene(scene)
             }
-            
             view.ignoresSiblingOrder = true
             
             view.showsFPS = true
             view.showsNodeCount = true
         }
     }
-
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
+    
 }
---
+++ FruityPenguins/Controllers/Scenes/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  FruityPenguins
+//
+//  Created by Neil Hiddink on 9/6/18.
+//  Copyright © 2018 Neil Hiddink. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+        
+    }
+    
+}
