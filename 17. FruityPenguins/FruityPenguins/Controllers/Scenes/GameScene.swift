b223a78b4c8e692094d8fad61622e462914987d1
Neil Hiddink <nhiddink2@gmail.com>
2018-09-14 10:50:14 -0400

    feat: (FruityPenguins) Add Background and Implement Helper Methods in GameScene

--- 17. FruityPenguins/FruityPenguins/Controllers/Scenes/GameScene.swift
+++ 17. FruityPenguins/FruityPenguins/Controllers/Scenes/GameScene.swift
@@ -11,9 +11,69 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    // MARK: Properties
+    
+    var scoreLabel: SKLabelNode!
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    var livesImages = [SKSpriteNode]()
+    var lives = 3
+    
+    var activeSliceBG: SKShapeNode!
+    var activeSliceFG: SKShapeNode!
+    
+    // MARK: Scene Life Cycle
+    
     override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "sliceBackground")
+        background.anchorPoint = CGPoint(x: 0, y: 0)
+        background.position = CGPoint(x: 0, y: 0)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
         
+        physicsWorld.gravity = CGVector(dx: 0, dy: -6)
+        physicsWorld.speed = 0.85
         
+        createScore()
+        createLives()
+        createSlices()
     }
     
+    // MARK: Helper Methods
+    
+    func createScore() {
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.text = "Score: 0"
+        scoreLabel.horizontalAlignmentMode = .left
+        scoreLabel.fontSize = 48
+        scoreLabel.position = CGPoint(x: 8, y: 8)
+        addChild(scoreLabel)
+    }
+    
+    func createLives() {
+        for i in 0 ..< 3 {
+            let spriteNode = SKSpriteNode(imageNamed: "sliceLife")
+            spriteNode.position = CGPoint(x: CGFloat(834 + (i * 70)), y: 720)
+            addChild(spriteNode)
+            livesImages.append(spriteNode)
+        }
+    }
+    
+    func createSlices() {
+        activeSliceBG = SKShapeNode()
+        activeSliceBG.zPosition = 2
+        activeSliceFG = SKShapeNode()
+        activeSliceFG.zPosition = 2
+        activeSliceBG.strokeColor = UIColor(red: 1, green: 0.9,
+                                            blue: 0, alpha: 1)
+        activeSliceBG.lineWidth = 9
+        activeSliceFG.strokeColor = UIColor.white
+        activeSliceFG.lineWidth = 5
+        addChild(activeSliceBG)
+        addChild(activeSliceFG)
+    }
 }
