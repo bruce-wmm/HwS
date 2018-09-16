93d424c45d50d83b91076f6f0502b9a05a491bd8
Neil Hiddink <nhiddink2@gmail.com>
2018-09-16 18:24:42 -0400

    feat: (FruityPenguins) Implement Touch Methods and Drawing

--- 17. FruityPenguins/FruityPenguins/Controllers/Scenes/GameScene.swift
+++ 17. FruityPenguins/FruityPenguins/Controllers/Scenes/GameScene.swift
@@ -25,6 +25,8 @@ class GameScene: SKScene {
     var activeSliceBG: SKShapeNode!
     var activeSliceFG: SKShapeNode!
     
+    var activeSlicePoints = [CGPoint]()
+    
     // MARK: Scene Life Cycle
     
     override func didMove(to view: SKView) {
@@ -68,12 +70,73 @@ class GameScene: SKScene {
         activeSliceBG.zPosition = 2
         activeSliceFG = SKShapeNode()
         activeSliceFG.zPosition = 2
-        activeSliceBG.strokeColor = UIColor(red: 1, green: 0.9,
-                                            blue: 0, alpha: 1)
+        activeSliceBG.strokeColor = UIColor(red: 1, green: 0.9, blue: 0, alpha: 1)
         activeSliceBG.lineWidth = 9
         activeSliceFG.strokeColor = UIColor.white
         activeSliceFG.lineWidth = 5
         addChild(activeSliceBG)
         addChild(activeSliceFG)
+    }
+    
+    func redrawActiveSlice() {
+
+        if activeSlicePoints.count < 2 {
+            activeSliceBG.path = nil
+            activeSliceFG.path = nil
+            return
+        }
+
+        while activeSlicePoints.count > 12 {
+            activeSlicePoints.remove(at: 0)
+        }
+
+        let path = UIBezierPath()
+        path.move(to: activeSlicePoints[0])
+        for i in 1 ..< activeSlicePoints.count {
+            path.addLine(to: activeSlicePoints[i])
+        }
+
+        activeSliceBG.path = path.cgPath
+        activeSliceFG.path = path.cgPath
+    }
+    
+    // MARK: Touch Methods
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event:
+        UIEvent?) {
+        super.touchesBegan(touches, with: event)
+
+        activeSlicePoints.removeAll(keepingCapacity: true)
+
+        if let touch = touches.first {
+            let location = touch.location(in: self)
+            activeSlicePoints.append(location)
+
+            redrawActiveSlice()
+
+            activeSliceBG.removeAllActions()
+            activeSliceFG.removeAllActions()
+
+            activeSliceBG.alpha = 1
+            activeSliceFG.alpha = 1
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        activeSlicePoints.append(location)
+        redrawActiveSlice()
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event:
+        UIEvent?) {
+        activeSliceBG.run(SKAction.fadeOut(withDuration: 0.25))
+        activeSliceFG.run(SKAction.fadeOut(withDuration: 0.25))
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with
+        event: UIEvent?) {
+        touchesEnded(touches, with: event)
     }
 }
