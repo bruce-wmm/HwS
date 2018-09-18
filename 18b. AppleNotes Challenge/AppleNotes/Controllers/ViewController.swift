cb82eab4f63508038cefb7585513af72b03118e3
Neil Hiddink <nhiddink2@gmail.com>
2018-09-18 16:38:31 -0400

    feat: (AppleNotes) Add and Organize Files - Project 18b Challenge

--- 18b. AppleNotes Challenge/AppleNotes/Controllers/ViewController.swift
+++ 18b. AppleNotes Challenge/AppleNotes/Controllers/ViewController.swift
@@ -1,29 +1,18 @@
 //
 //  ViewController.swift
-//  CapitalCities
+//  AppleNotes
 //
 //  Created by Neil Hiddink on 9/18/18.
 //  Copyright © 2018 Neil Hiddink. All rights reserved.
 //
 
 import UIKit
-import MapKit
-
-// MARK: ViewController: UIViewController
 
 class ViewController: UIViewController {
 
-    // MARK: IB Outlets
-    
-    @IBOutlet var mapView: MKMapView!
-    
-    // MARK: View Life Cycle
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
-
 
 }
 
