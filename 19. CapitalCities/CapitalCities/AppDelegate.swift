d7258ccd18e0847eb5911c171e1ac61272209a16
Neil Hiddink <nhiddink2@gmail.com>
2018-09-18 16:26:06 -0400

    feat: (CapitalCities) Add and Organize Files for Project 19

---
+++ 19. CapitalCities/CapitalCities/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  CapitalCities
+//
+//  Created by Neil Hiddink on 9/18/18.
+//  Copyright © 2018 Neil Hiddink. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
---
+++ 19. CapitalCities/CapitalCities/Controllers/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CapitalCities
+//
+//  Created by Neil Hiddink on 9/18/18.
+//  Copyright © 2018 Neil Hiddink. All rights reserved.
+//
+
+import UIKit
+
+// MARK: ViewController: UIViewController
+
+class ViewController: UIViewController {
+
+    // MARK: View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
