8e97c79933501fe194825f4a87a2bb8e674548c0
Neil Hiddink <nhiddink2@gmail.com>
2018-10-02 06:00:49 -0400

    feat: (Detect-A-Beacon) Add Project 22 Files

---
+++ 22. Detect-A-Beacon/Detect-A-Beacon/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Detect-A-Beacon
+//
+//  Created by Neil Hiddink on 10/2/18.
+//  Copyright © 2018 Neil Hiddink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
