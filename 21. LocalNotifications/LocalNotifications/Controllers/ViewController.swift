5bcfebfa9bbc85518ddcd870bf1271185a53c042
Neil Hiddink <nhiddink2@gmail.com>
2018-10-02 05:00:37 -0400

    feat: (Local Notifications) Add NavigationController, Bar Buttons, Register Events

--- 21. LocalNotifications/LocalNotifications/Controllers/ViewController.swift
+++ 21. LocalNotifications/LocalNotifications/Controllers/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import UserNotifications
+
+// MARK: - ViewController: UIViewController
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    
+    var center = UNUserNotificationCenter.current()
+    
+    // MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
     }
-
+    
+    // MARK: Helper Methods
+    
+    @objc func registerLocal() {
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if granted {
+                print("Success")
+            } else {
+                print("Something went wrong...")
+            }
+        }
+    }
+    
+    @objc func scheduleLocal() {
+    
+    }
 
 }
 
