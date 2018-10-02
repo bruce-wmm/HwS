5cfd453753934e295114d6bde1fd4f2b2d0a3c4a
Neil Hiddink <nhiddink2@gmail.com>
2018-10-02 05:38:18 -0400

    feat: (Local Notifications) Implement didReceive Method of UNUserNotificationCenterDelegate

--- 21. LocalNotifications/LocalNotifications/Controllers/ViewController.swift
+++ 21. LocalNotifications/LocalNotifications/Controllers/ViewController.swift
@@ -13,11 +13,6 @@ import UserNotifications
 
 class ViewController: UIViewController {
     
-    // MARK: - Properties
-    
-    let center = UNUserNotificationCenter.current()
-    let content = UNMutableNotificationContent()
-    
     // MARK: - View Life Cycle
     
     override func viewDidLoad() {
@@ -30,6 +25,7 @@ class ViewController: UIViewController {
     // MARK: Helper Methods
     
     @objc func registerLocal() {
+        let center = UNUserNotificationCenter.current()
         center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
             if granted {
                 print("Success")
@@ -40,8 +36,13 @@ class ViewController: UIViewController {
     }
     
     @objc func scheduleLocal() {
+        registerCategories()
+        
+        let center = UNUserNotificationCenter.current()
+        
         center.removeAllPendingNotificationRequests()
-
+        
+        let content = UNMutableNotificationContent()
         content.title = "Late wake up call"
         content.body = "The early bird catches the worm, but the second mouse gets the cheese."
         content.categoryIdentifier = "alarm"
@@ -55,6 +56,32 @@ class ViewController: UIViewController {
         let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
         center.add(request)
     }
-
+    
+    func registerCategories() {
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        let show = UNNotificationAction(identifier: "show", title: "Tell me more...", options: .foreground)
+        let category = UNNotificationCategory(identifier: "alarm", actions: [show], intentIdentifiers: [])
+        center.setNotificationCategories([category])
+    }
 }
 
+extension ViewController: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        
+        if let customData = userInfo["customData"] as? String {
+            print("Custom data received: \(customData)")
+            
+            switch response.actionIdentifier {
+                case UNNotificationDefaultActionIdentifier: // user swiped to unlock
+                    print("Default identifier")
+                case "show": // user tapped "show more info..." button
+                    print("Show more information...")
+                default:
+                    break
+            }
+            completionHandler()
+        }
+    }
+}
