973b9665e2f1e0fc97e776853a99104daab20780
Neil Hiddink <nhiddink2@gmail.com>
2018-10-02 06:17:33 -0400

    feat: (Detect-A-Beacon) Handle Privacy for Location Feature

--- 22. Detect-A-Beacon/Detect-A-Beacon/Controllers/ViewController.swift
+++ 22. Detect-A-Beacon/Detect-A-Beacon/Controllers/ViewController.swift
@@ -7,13 +7,41 @@
 //
 
 import UIKit
+import CoreLocation
+
+// MARK: - ViewController: UIViewController
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    
+    var locationManager: CLLocationManager!
+    
+    // MARK: - IB Outlets
+    
+    @IBOutlet var distanceReading: UILabel!
+    
+    // MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
     }
 
+}
+
+// MARK: - ViewController: CLLocationManagerDelegate
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways && CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+            if CLLocationManager.isRangingAvailable() {
+                
+            }
+        }
+    }
 }
 
