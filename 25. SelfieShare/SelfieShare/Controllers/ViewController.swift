053906f9f377efb7828af9f5c6487e6a0e66812c
Neil Hiddink <nhiddink2@gmail.com>
2018-10-21 08:55:49 -0400

    feat: (SelfieShare) Add Extensions to ViewController

--- 25. SelfieShare/SelfieShare/Controllers/ViewController.swift
+++ 25. SelfieShare/SelfieShare/Controllers/ViewController.swift
@@ -12,6 +12,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // MARK: - Properties
+    
+    var images = [UIImage]()
+    
     // MARK: - IB Outlets
     
     @IBOutlet var collectionView: UICollectionView!
@@ -21,6 +25,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        title = "Selfie Share"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
+    }
+    
+    // MARK: - Helper Methods
+    
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
     }
 
 }
@@ -38,13 +53,29 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     // MARK: Data Source
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return images.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SelfieCell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageView", for: indexPath)
+        
+        if let imageView = cell.viewWithTag(1000) as? UIImageView {
+            imageView.image = images[indexPath.item]
+        }
+        
         return cell
     }
     
+}
+
+// MARK: - ViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate
+
+extension ViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    private func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        images.insert(image, at: 0)
+        collectionView?.reloadData()
+    }
 }
 
