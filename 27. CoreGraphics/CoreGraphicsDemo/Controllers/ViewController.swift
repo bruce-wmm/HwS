172475c493e46b726b3125d19fb61faa08750806
Neil Hiddink <nhiddink2@gmail.com>
2018-10-24 22:40:08 -0400

    feat/fix: (CoreGraphicsDemo) Implement drawCheckerBoard Method and Fix Cropping Issue(s)

--- 27. CoreGraphics/CoreGraphicsDemo/Controllers/ViewController.swift
+++ 27. CoreGraphics/CoreGraphicsDemo/Controllers/ViewController.swift
@@ -34,7 +34,7 @@ class ViewController: UIViewController {
         let renderer = UIGraphicsImageRenderer(size: CGSize(width:
             512, height: 512))
         let img = renderer.image { ctx in
-            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
+            let rectangle = CGRect(x: 5, y: 5, width: 502, height: 502)
             ctx.cgContext.setFillColor(UIColor.red.cgColor)
             ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
             ctx.cgContext.setLineWidth(10)
@@ -48,12 +48,27 @@ class ViewController: UIViewController {
         let renderer = UIGraphicsImageRenderer(size: CGSize(width:
             512, height: 512))
         let img = renderer.image { ctx in
-            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
+            let rectangle = CGRect(x: 5, y: 5, width: 502, height: 502)
             ctx.cgContext.setFillColor(UIColor.red.cgColor)
             ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
             ctx.cgContext.setLineWidth(10)
             ctx.cgContext.addEllipse(in: rectangle)
             ctx.cgContext.drawPath(using: .fillStroke)
+        }
+        imageView.image = img
+    }
+    
+    func drawCheckerboard() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        let img = renderer.image { ctx in
+            ctx.cgContext.setFillColor(UIColor.black.cgColor)
+            for row in 0 ..< 8 {
+                for col in 0 ..< 8 {
+                    if (row + col) % 2 == 0 {
+                        ctx.cgContext.fill(CGRect(x: col * 64, y: row * 64, width: 64, height: 64))
+                    }
+                }
+            }
         }
         imageView.image = img
     }
@@ -70,6 +85,8 @@ class ViewController: UIViewController {
             drawRectangle()
         case 1:
             drawCircle()
+        case 2:
+            drawCheckerboard()
         default:
             break
         }
