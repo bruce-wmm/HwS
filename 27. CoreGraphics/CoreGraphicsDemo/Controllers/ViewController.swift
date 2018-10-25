8c41363136d5c420329a89e48f912b910191caf1
Neil Hiddink <nhiddink2@gmail.com>
2018-10-24 20:17:24 -0400

    feat: (CoreGraphicsDemo) Add Project 27 Files and Organize

---
+++ 27. CoreGraphics/CoreGraphicsDemo/Controllers/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  CoreGraphics
+//
+//  Created by Neil Hiddink on 10/24/18.
+//  Copyright © 2018 Neil Hiddink. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - ViewController: UIViewController
+
+class ViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    var currentDrawType = 0
+    
+    // MARK: - IB Outlets
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        drawRectangle()
+    }
+    
+    // MARK: Helper Methods
+    
+    func drawRectangle() {
+        
+    }
+    
+    // MARK: - IB Actions
+    
+    @IBAction func redrawTapped(_ sender: UIButton) {
+        currentDrawType += 1
+        if currentDrawType > 5 {
+            currentDrawType = 0
+        }
+        switch currentDrawType {
+        case 0:
+            drawRectangle()
+        default:
+            break
+        }
+    }
+    
+}
+
