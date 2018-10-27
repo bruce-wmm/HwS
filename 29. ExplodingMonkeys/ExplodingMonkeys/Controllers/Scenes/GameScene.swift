24c660502f1e8fd5c317d2b9b8288f7f5add21a6
Neil Hiddink <nhiddink2@gmail.com>
2018-10-27 16:06:32 -0400

    feat: (ExplodingMonkeys) Add Project 29 Files and Organize

---
+++ 29. ExplodingMonkeys/ExplodingMonkeys/Controllers/Scenes/GameScene.swift
@@ -0,0 +1,40 @@
+//
+//  GameScene.swift
+//  ExplodingMonkeys
+//
+//  Created by Neil Hiddink on 10/27/18.
+//  Copyright © 2018 Neil Hiddink. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+// MARK: - GameScene: SKScene
+
+class GameScene: SKScene {
+    
+    // MARK: - Scene Life Cycle
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+
+    }
+    
+    // MARK: - Touch Methods
+    
+    func touchDown(at location: CGPoint) {
+       
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first { self.touchDown(at: touch.location(in: self)) }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first { self.touchDown(at: touch.location(in: self)) }
+    }
+    
+}
