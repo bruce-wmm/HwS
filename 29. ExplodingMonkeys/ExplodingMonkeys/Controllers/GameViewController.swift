e72f2d2cf843368670112d639b70ad30811b46ad
Neil Hiddink <nhiddink2@gmail.com>
2018-10-28 16:12:39 -0400

    feat: (ExplodingMonkeys) Implement createBuildings for Random Building Setup

--- 29. ExplodingMonkeys/ExplodingMonkeys/Controllers/GameViewController.swift
+++ 29. ExplodingMonkeys/ExplodingMonkeys/Controllers/GameViewController.swift
@@ -26,7 +26,7 @@ class GameViewController: UIViewController {
         
         if let view = self.view as! SKView? {
             if let scene = SKScene(fileNamed: "GameScene") {
-                scene.scaleMode = .aspectFill
+                scene.scaleMode = .aspectFit
                 
                 view.presentScene(scene)
             }
--- 29. ExplodingMonkeys/ExplodingMonkeys/Controllers/Scenes/GameScene.swift
+++ 29. ExplodingMonkeys/ExplodingMonkeys/Controllers/Scenes/GameScene.swift
@@ -13,14 +13,36 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    // MARK: - Properties
+    
+    var buildings = [BuildingNode]()
+
     // MARK: - Scene Life Cycle
     
     override func didMove(to view: SKView) {
+        super.didMove(to: view)
         
+        backgroundColor = UIColor(hue: 0.669, saturation: 0.99, brightness: 0.67, alpha: 1)
+        createBuildings()
     }
     
     override func update(_ currentTime: TimeInterval) {
 
+    }
+    
+    // MARK: - Helper Methods
+    
+    func createBuildings() {
+        var currentX: CGFloat = -15
+        while currentX < 1024 {
+            let size = CGSize(width: RandomInt(min: 2, max: 4) * 40, height: RandomInt(min: 300, max: 600))
+            currentX += size.width + 2
+            let building = BuildingNode(color: UIColor.red, size: size)
+            building.position = CGPoint(x: currentX - (size.width / 2), y: size.height / 2)
+            building.setup()
+            addChild(building)
+            buildings.append(building)
+        }
     }
     
     // MARK: - Touch Methods
