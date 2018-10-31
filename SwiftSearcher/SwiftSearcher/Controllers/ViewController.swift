7a25305eb06afc1fad11918e5d0efc0dcc8361a9
Neil Hiddink <nhiddink2@gmail.com>
2018-10-30 22:55:02 -0400

    feat: (SwiftSearcher) Add Project 32 Files and Organize - App Icon, LaunchScreen, etc.

---
+++ SwiftSearcher/SwiftSearcher/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftSearcher
+//
+//  Created by Neil Hiddink on 10/30/18.
+//  Copyright © 2018 Neil Hiddink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
