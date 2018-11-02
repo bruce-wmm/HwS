e84980e21f6755e217333b7831018e3a85f12a10
Neil Hiddink <nhiddink2@gmail.com>
2018-11-01 22:24:40 -0400

    feat: (SwiftSearcher) Complete App - Deep Linking to Core Spotlight

--- 32. SwiftSearcher/SwiftSearcher/AppDelegate.swift
+++ 32. SwiftSearcher/SwiftSearcher/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreSpotlight
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        if userActivity.activityType == CSSearchableItemActionType {
+            if let uniqueIdentifier = userActivity.userInfo?[CSSearchableItemActivityIdentifier] as? String {
+                if let navigationController = window?.rootViewController as? UINavigationController {
+                    if let viewController = navigationController.topViewController as? ViewController {
+                        viewController.showProject(Int(uniqueIdentifier)!)
+                    }
+                }
+            }
+        }
         return true
     }
 
