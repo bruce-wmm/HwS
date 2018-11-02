fc3b1e927f52795d95950d835f64cd52f30b5fc2
Neil Hiddink <nhiddink2@gmail.com>
2018-11-01 22:16:58 -0400

    feat: (SwiftSearcher) Implement Add and Delete Favorites

--- 32. SwiftSearcher/SwiftSearcher/Controllers/ViewController.swift
+++ 32. SwiftSearcher/SwiftSearcher/Controllers/ViewController.swift
@@ -8,10 +8,19 @@
 
 import UIKit
 import SafariServices
+import CoreSpotlight
+import MobileCoreServices
+
+// MARK: - ViewController: UIViewController
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    
     var projects = [[String]]()
+    var favorites = [Int]()
+    
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +33,12 @@ class ViewController: UIViewController {
         projects.append(["Project 6: Auto Layout", "Get to grips with Auto Layout using practical examples and code"])
         projects.append(["Project 7: Whitehouse Petitions", "JSON, Data, UITabBarController"])
         projects.append(["Project 8: 7 Swifty Words", "addTarget(), enumerated(), count, index(of:), property observers, range operators."])
+        
+        if let savedFavorites = UserDefaults.standard.object(forKey: "favorites") as? [Int] {
+            favorites = savedFavorites
+        }
+        tableView.isEditing = true
+        tableView.allowsSelectionDuringEditing = true
     }
     
     // MARK: - Helper Methods
@@ -49,11 +64,65 @@ class ViewController: UIViewController {
     
 }
 
+// MARK: - ViewController: UITableViewDelegate, UITableViewDataSource
+
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    // MARK: Delegate
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         showProject(indexPath.row)
     }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        if favorites.contains(indexPath.row) {
+            return .delete
+        } else {
+            return .insert
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .insert {
+            favorites.append(indexPath.row)
+            index(item: indexPath.row)
+        } else {
+            if let index = favorites.index(of: indexPath.row) {
+                favorites.remove(at: index)
+                deindex(item: indexPath.row)
+            }
+        }
+        UserDefaults.standard.set(favorites, forKey: "favorites")
+        tableView.reloadRows(at: [indexPath], with: .none)
+    }
+    
+    func index(item: Int) {
+        let project = projects[item]
+        let attributeSet = CSSearchableItemAttributeSet(itemContentType: kUTTypeText as String)
+        attributeSet.title = project[0]
+        attributeSet.contentDescription = project[1]
+        let item = CSSearchableItem(uniqueIdentifier: "\(item)", domainIdentifier: "com.hackingwithswift", attributeSet: attributeSet)
+        item.expirationDate = Date.distantFuture // i.e. never expires
+        CSSearchableIndex.default().indexSearchableItems([item]) { error in
+            if let error = error {
+                print("Indexing error: \(error.localizedDescription)")
+            } else {
+                print("Search item successfully indexed!")
+            }
+        }
+    }
+    
+    func deindex(item: Int) {
+        CSSearchableIndex.default().deleteSearchableItems(withIdentifiers: ["\(item)"]) { error in
+            if let error = error {
+                print("Deindexing error: \(error.localizedDescription)")
+            } else {
+                print("Search item successfully removed!")
+            }
+        }
+    }
+    
+    // MARK: Data Source
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return projects.count
@@ -62,6 +131,13 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.attributedText = makeAttributedString(title: projects[indexPath.row][0], subtitle: projects[indexPath.row][1])
+        
+        if favorites.contains(indexPath.row) {
+            cell.editingAccessoryType = .checkmark
+        } else {
+            cell.editingAccessoryType = .none
+        }
+        
         return cell
     }
 
