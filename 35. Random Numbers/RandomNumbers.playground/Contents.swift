3409908810c9b9d2bf99db367519536fb612802e
Neil Hiddink <nhiddink2@gmail.com>
2018-11-18 12:25:37 -0500

    feat: (RandomNumbers) Shuffling Arrays with GameplayKit

--- 35. Random Numbers/RandomNumbers.playground/Contents.swift
+++ 35. Random Numbers/RandomNumbers.playground/Contents.swift
@@ -1,5 +1,9 @@
 import UIKit
 
+//////////////////////////////////////////////
+// MARK: - Random Numbers in Swift (Built-in)
+//////////////////////////////////////////////
+
 // Swift has built-in support for random numbers of various types
 let int1 = Int.random(in: 0...10)
 let int2 = Int.random(in: 0..<10)
@@ -24,6 +28,10 @@ func RandomInt(min: Int, max: Int) -> Int {
     min
 }
 
+/////////////////////////////////////////
+// MARK: - Random Numbers in GameplayKit
+/////////////////////////////////////////
+
 // Generating random numbers with GameplayKit: GKRandomSource
 import GameplayKit
 
@@ -33,6 +41,9 @@ print(GKRandomSource.sharedRandom().nextInt())
 // In a range, identical to arc4random
 print(GKRandomSource.sharedRandom().nextInt(upperBound: 6))
 
+///////////////////////////////////////////////////////
+// MARK: - Other Options for Generating Random Numbers
+///////////////////////////////////////////////////////
 
 // GKARC4RandomSource and other GameplayKit options
 let cong = GKLinearCongruentialRandomSource() // highest performance + lowest randomness
@@ -50,7 +61,9 @@ mersenne.nextInt(upperBound: 20)
 // Apple suggests 769 values at a minimum
 arc4.dropValues(1024)
 
-// GKRandomDistribution, GKShuffledDistribution and GKGaussianDistribution
+///////////////////////////////////////////////////////////////////////////////////
+// MARK: - GKRandomDistribution, GKShuffledDistribution and GKGaussianDistribution
+///////////////////////////////////////////////////////////////////////////////////
 
 let d6 = GKRandomDistribution.d6()
 d6.nextInt()
@@ -76,3 +89,48 @@ print(shuffled)
 let gauss = GKGaussianDistribution(lowestValue: 1, highestValue: 20)
 print(gauss)
 
+/////////////////////////////////////////////////
+// MARK: - Options for Randomly Shuffling Arrays
+/////////////////////////////////////////////////
+
+// Fisher-Yates Array Shuffling Algorithm adapted for Swift by Nate Cook
+extension Array {
+    mutating func shuffle() {
+        // for all items in the array
+        for i in 0..<(count - 1) {
+            // generate a random number up to the last index in the array
+            // and add the current index's value to it.
+            let j = Int(arc4random_uniform(UInt32(count - i))) + i
+            // swap the values at the two indices
+            swapAt(i, j)
+            
+            /* Example:
+                          0  1  2  3  4  5  6  7  8  9
+             let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+             ITERATION #1:
+                i = 0
+                j = (5 - 0) + 0 = 5
+                [5, 1, 2, 3, 4, 0, 6, 7, 8, 9]
+             
+             NOTE: This algorithm is commonly used for card shuffling, etc.
+            */
+        }
+    }
+}
+
+// Similarly, with GameplayKit you can randomize a fake lottery using:
+let lotteryBalls = [Int](1...49)
+let shuffledBalls = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: lotteryBalls)
+print(shuffledBalls[0])
+print(shuffledBalls[1])
+print(shuffledBalls[2])
+print(shuffledBalls[3])
+print(shuffledBalls[4])
+print(shuffledBalls[5])
+
+// Random Order Shuffling with a fixed seed value of 1001
+
+let fixedLotteryBalls = [Int](1...49)
+// Seed with another random number instead of using 1001 to remove predictability in future states
+let fixedShuffledBalls = GKMersenneTwisterRandomSource(seed: 1001).arrayByShufflingObjects(in: fixedLotteryBalls)
+print(fixedShuffledBalls[0])
