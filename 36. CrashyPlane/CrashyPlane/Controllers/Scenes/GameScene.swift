89e0d366ecfed6b1c850771c8bc37b48ee2196ef
Neil Hiddink <nhiddink2@gmail.com>
2018-11-21 11:12:22 -0600

    feat: (CrashyPlane) Add Player to Scene

--- 36. CrashyPlane/CrashyPlane/Controllers/Scenes/GameScene.swift
+++ 36. CrashyPlane/CrashyPlane/Controllers/Scenes/GameScene.swift
@@ -21,7 +21,22 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         super.didMove(to: view)
         
-        
+        createPlayer()
+    }
+    
+    // MARK: - Helper Methods
+    
+    func createPlayer() {
+        let playerTexture = SKTexture(imageNamed: "player-1")
+        player = SKSpriteNode(texture: playerTexture)
+        player.zPosition = 10
+        player.position = CGPoint(x: frame.width / 6, y: frame.height * 0.75)
+        addChild(player)
+        let frame2 = SKTexture(imageNamed: "player-2")
+        let frame3 = SKTexture(imageNamed: "player-3")
+        let animation = SKAction.animate(with: [playerTexture, frame2, frame3, frame2], timePerFrame: 0.01)
+        let runForever = SKAction.repeatForever(animation)
+        player.run(runForever)
     }
     
     // MARK: - Touch Methods
