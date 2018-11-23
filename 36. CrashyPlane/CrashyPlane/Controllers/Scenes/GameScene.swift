523b450e659fa6769466493e6e9b44c512d58392
Neil Hiddink <nhiddink2@gmail.com>
2018-11-23 13:36:58 -0600

    feat: (CrashyPlane) Add Sky and Background

--- 36. CrashyPlane/CrashyPlane/Controllers/Scenes/GameScene.swift
+++ 36. CrashyPlane/CrashyPlane/Controllers/Scenes/GameScene.swift
@@ -22,6 +22,8 @@ class GameScene: SKScene {
         super.didMove(to: view)
         
         createPlayer()
+        createSky()
+        createBackground()
     }
     
     // MARK: - Helper Methods
@@ -37,6 +39,35 @@ class GameScene: SKScene {
         let animation = SKAction.animate(with: [playerTexture, frame2, frame3, frame2], timePerFrame: 0.01)
         let runForever = SKAction.repeatForever(animation)
         player.run(runForever)
+    }
+    
+    func createSky() {
+        let topSky = SKSpriteNode(color: UIColor(hue: 0.55, saturation: 0.14, brightness: 0.97, alpha: 1), size:
+        CGSize(width: frame.width, height: frame.height * 0.67))
+        topSky.anchorPoint = CGPoint(x: 0.5, y: 1)
+        let bottomSky = SKSpriteNode(color: UIColor(hue: 0.55, saturation: 0.16, brightness: 0.96, alpha: 1), size:
+        CGSize(width: frame.width, height: frame.height * 0.33))
+        bottomSky.anchorPoint = CGPoint(x: 0.5, y: 1)
+        
+        topSky.position = CGPoint(x: frame.midX, y: frame.height)
+        bottomSky.position = CGPoint(x: frame.midX, y: bottomSky.frame.height)
+        
+        addChild(topSky)
+        addChild(bottomSky)
+        
+        bottomSky.zPosition = -40
+        topSky.zPosition = -40
+    }
+    
+    func createBackground() {
+        let backgroundTexture = SKTexture(imageNamed: "background")
+        for i in 0...1 {
+            let background = SKSpriteNode(texture: backgroundTexture)
+            background.zPosition = -30
+            background.anchorPoint = CGPoint.zero
+            background.position = CGPoint(x: (backgroundTexture.size().width * CGFloat(i)) - CGFloat(1 * i), y: 100)
+            addChild(background)
+        }
     }
     
     // MARK: - Touch Methods
