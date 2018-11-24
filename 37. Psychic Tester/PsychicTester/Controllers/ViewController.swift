f6af1b8201b9541b2cee1632822dc00dc9b81d28
Neil Hiddink <nhiddink2@gmail.com>
2018-11-24 17:13:49 -0500

    feat: (PsychicTester) Add Project 37 Files and Organize - App Icon, Launch Screen, etc

---
+++ 37. Psychic Tester/PsychicTester/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PsychicTester
+//
+//  Created by Neil Hiddink on 11/24/18.
+//  Copyright © 2018 Neil Hiddink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
