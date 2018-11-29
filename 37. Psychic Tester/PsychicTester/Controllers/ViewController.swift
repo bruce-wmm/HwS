54ee3d1f4592a1af4e9c72bc29e11377db2ba86e
Neil Hiddink <nhiddink2@gmail.com>
2018-11-28 22:03:37 -0500

    feat: (PsychicTester) Add loadCards() Method to ViewController.swift

--- 37. Psychic Tester/PsychicTester/Controllers/ViewController.swift
+++ 37. Psychic Tester/PsychicTester/Controllers/ViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import GameplayKit
 
 // MARK: - ViewController: UIViewController
 
 class ViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    var allCards = [CardViewController]()
+    
     // MARK: - IB Outlets
     
     @IBOutlet var cardContainer: UIView!
@@ -21,8 +26,58 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        loadCards()
     }
 
-
+    // MARK: - Helper Methods
+    
+    @objc func loadCards() {
+        
+        for card in allCards {
+            card.view.removeFromSuperview()
+            card.removeFromParent()
+        }
+        allCards.removeAll(keepingCapacity: true)
+        
+        let positions = [
+            CGPoint(x: 75, y: 85),
+            CGPoint(x: 185, y: 85),
+            CGPoint(x: 295, y: 85),
+            CGPoint(x: 405, y: 85),
+            CGPoint(x: 75, y: 235),
+            CGPoint(x: 185, y: 235),
+            CGPoint(x: 295, y: 235),
+            CGPoint(x: 405, y: 235)
+        ]
+        
+        let circle = UIImage(named: "cardCircle")!
+        let cross = UIImage(named: "cardCross")!
+        let lines = UIImage(named: "cardLines")!
+        let square = UIImage(named: "cardSquare")!
+        let star = UIImage(named: "cardStar")!
+        
+        var images = [circle, circle, cross, cross, lines, lines, square, star]
+        images = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: images) as! [UIImage]
+        
+        for (index, position) in positions.enumerated() {
+            
+            let card = CardViewController()
+            card.delegate = self
+            
+            addChild(card)
+            cardContainer.addSubview(card.view)
+            card.didMove(toParent: self)
+            
+            card.view.center = position
+            card.front.image = images[index]
+                
+            if card.front.image == star {
+                card.isCorrect = true
+            }
+            
+            allCards.append(card)
+        }
+        
+    }
 }
 
