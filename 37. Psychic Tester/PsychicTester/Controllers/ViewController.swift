fedeaa423094264f10ffd69c074ba51ddd96e5e7
Neil Hiddink <nhiddink2@gmail.com>
2018-11-28 21:48:44 -0500

    feat: (PsychicTester) Configure UI in Main.storyboard

--- 37. Psychic Tester/PsychicTester/Controllers/ViewController.swift
+++ 37. Psychic Tester/PsychicTester/Controllers/ViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
+// MARK: - ViewController: UIViewController
+
 class ViewController: UIViewController {
 
+    // MARK: - IB Outlets
+    
+    @IBOutlet var cardContainer: UIView!
+   
+    // MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
 
