18daa3f86cc3e79c47332957209783f17ec176e7
Neil Hiddink <nhiddink2@gmail.com>
2018-11-30 18:27:57 -0500

    feat: (PsychicTester) Implement Interface.storyboard - Link to Implemented InterfaceController.swift

--- 37. Psychic Tester/PsychicTester WatchKit Extension/InterfaceController.swift
+++ 37. Psychic Tester/PsychicTester WatchKit Extension/InterfaceController.swift
@@ -8,24 +8,54 @@
 
 import WatchKit
 import Foundation
+import WatchConnectivity
 
+// MARK: - InterfaceController: WKInterfaceController
 
 class InterfaceController: WKInterfaceController {
 
+    // MARK: - IB Outlets
+    
+    @IBOutlet var welcomeText: WKInterfaceLabel!
+    @IBOutlet var hideButton: WKInterfaceButton!
+    
+    // MARK: - Interface Life Cycle
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
-        // Configure interface objects here.
+        if WCSession.isSupported() {
+            let session = WCSession.default
+            session.delegate = self
+            session.activate()
+        }
     }
     
     override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        
     }
     
     override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+        
     }
+    
+    // MARK: - IB Actions
 
+    @IBAction func hideWelcomeText() {
+        welcomeText.setHidden(true)
+        hideButton.setHidden(true)
+    }
+}
+
+// MARK: - InterfaceController: WCSessionDelegate
+
+extension InterfaceController: WCSessionDelegate {
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) { }
+    
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any], replyHandler: @escaping ([String : Any]) -> Void) {
+        WKInterfaceDevice().play(.click)
+    }
 }
