557b1b757a3a75d4f40ec5319444f2bcdc771c57
Neil Hiddink <nhiddink2@gmail.com>
2018-12-02 17:44:07 -0500

    feat: (GitHubCommits) Add Project 38 Files and Organize - App Icon, Launch Screen, etc.

---
+++ GitHubCommits/GitHubCommits/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubCommits
+//
+//  Created by Neil Hiddink on 12/2/18.
+//  Copyright © 2018 Neil Hiddink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
