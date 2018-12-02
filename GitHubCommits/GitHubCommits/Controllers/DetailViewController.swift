083ab3a8716cbbac6135f672dce6d22d2af6a4a1
Neil Hiddink <nhiddink2@gmail.com>
2018-12-02 18:03:16 -0500

    feat: (GitHubCommits) Configure Initial UI in Main.storyboard

---
+++ GitHubCommits/GitHubCommits/Controllers/DetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DetailViewController.swift
+//  GitHubCommits
+//
+//  Created by Neil Hiddink on 12/2/18.
+//  Copyright © 2018 Neil Hiddink. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - DetailViewController: UIViewController
+
+class DetailViewController: UIViewController {
+
+    // MARK: IB Outlets
+    
+    @IBOutlet var detailLabel: UILabel!
+    
+    // MARK: View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
--- GitHubCommits/GitHubCommits/Controllers/ViewController.swift
+++ GitHubCommits/GitHubCommits/Controllers/ViewController.swift
@@ -8,12 +8,47 @@
 
 import UIKit
 
+// MARK: - ViewController: UIViewController
+
 class ViewController: UIViewController {
 
+    // MARK: IB Outlets
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
+}
+
+// MARK: - ViewController: UITableViewDelegate, UITableViewDataSource
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    // MARK: Delegate
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    // MARK: Data Source
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Commit", for: indexPath)
+        return cell
+    }
+    
 }
 
