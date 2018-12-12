114d9c82dd4cfde5aa1e9be481c5d159a3ffab12
Neil Hiddink <nhiddink2@gmail.com>
2018-12-12 09:53:46 -0500

    build: (Swift Unit Tests) Run Unit Tests

---
+++ 39. Swift Unit Tests/SwiftUnitTests/Models/PlayData.swift
@@ -0,0 +1,15 @@
+//
+//  PlayData.swift
+//  SwiftUnitTests
+//
+//  Created by Neil Hiddink on 12/11/18.
+//  Copyright © 2018 Neil Hiddink. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - PlayData
+
+class PlayData {
+    var allWords = [String]()
+}
