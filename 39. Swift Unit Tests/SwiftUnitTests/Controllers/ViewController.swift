79d202cbf3640bc90f8d38ffac21605a8b946780
Neil Hiddink <nhiddink2@gmail.com>
2018-12-11 22:42:42 -0500

    feat: (Swift Unit Tests) Add Project 39 Files and Organize - App Icon, Launch Screen, etc

---
+++ 39. Swift Unit Tests/SwiftUnitTests/Controllers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SwiftUnitTests
+//
+//  Created by Neil Hiddink on 12/11/18.
+//  Copyright © 2018 Neil Hiddink. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - ViewController: UIViewController 
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
