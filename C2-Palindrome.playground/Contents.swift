92d6824ce81e2ff2fe168e97bfe1dbcf841e277c
Neil Hiddink <nhiddink2@gmail.com>
2019-01-03 19:00:08 -0500

    feat: (Strings) Complete Challenge 2

---
+++ C2-Palindrome.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+// Write a function that accepts a String as its only parameter and
+// returns true if the string reads the same when reversed, ignoring case.
+
+func checkPalindrome(word: String) -> Bool {
+    return word.lowercased() == String(word.reversed()).lowercased()
+}
+
+// Tests
+
+assert(checkPalindrome(word: "rotator") == true)
+assert(checkPalindrome(word: "Rats live on no evil star") == true)
+assert(checkPalindrome(word: "Never odd or even") == false)
+assert(checkPalindrome(word: "Hello, world") == false)
+print("All tests passed!")
