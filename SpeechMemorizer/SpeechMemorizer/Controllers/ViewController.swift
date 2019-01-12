8ae7eefb0e7db03fbed64d85d92c236265129809
Neil Hiddink <nhiddink2@gmail.com>
2019-01-12 15:02:45 -0500

    feat: (SpeechMemorizer) Starter Code + Initial UI
    Organize project files, add app icon, initial UI and code.

---
+++ SpeechMemorizer/SpeechMemorizer/Controllers/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  SpeechMemorizer
+//
+//  Created by Neil Hiddink on 1/12/19.
+//  Copyright © 2019 Neil Hiddink. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - ViewController: UIViewController
+
+class ViewController: UIViewController {
+
+    // MARK: Properties
+    
+    var speeches = [SpeechItem]()
+    
+    // MARK: IB Outlets
+    
+    @IBOutlet var tableView: UITableView!
+    // MARK: View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        guard let url = Bundle.main.url(forResource: "Speeches", withExtension: ".json") else {
+            fatalError("Can't find JSON.")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Unable to load JSON.")
+        }
+        
+        let decoder = JSONDecoder()
+        
+        guard let savedItems = try? decoder.decode([SpeechItem].self, from: data) else {
+            fatalError("Failed to decode JSON.")
+        }
+        
+        speeches = savedItems
+    }
+
+
+}
+
+// MARK: - ViewController: UITableViewDelegate, UITableViewDataSource
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    // MARK: Delegate
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    // MARK: Data Source
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return speeches.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let speech = speeches[indexPath.row]
+        cell.textLabel?.text = speech.title
+        cell.detailTextLabel?.text = "\(speech.author) + \(speech.date)"
+        return cell
+    }
+    
+}
---
+++ SpeechMemorizer/SpeechMemorizer/Models/SpeechItem.swift
@@ -0,0 +1,16 @@
+//
+//  SpeechItem.swift
+//  SpeechMemorizer
+//
+//  Created by Neil Hiddink on 1/12/19.
+//  Copyright © 2019 Neil Hiddink. All rights reserved.
+//
+
+import Foundation
+
+struct SpeechItem: Codable {
+    var title: String
+    var text: String
+    var author: String
+    var date: String
+}
