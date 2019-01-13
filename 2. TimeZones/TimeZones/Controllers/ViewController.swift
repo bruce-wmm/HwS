471d7241caa437e8c5ad851020d0cc9e9a1aa002
Neil Hiddink <nhiddink2@gmail.com>
2019-01-13 13:22:54 -0500

    feat: (TimeZones) Add Methods to Load and Save JSON Data

--- 2. TimeZones/TimeZones/Controllers/ViewController.swift
+++ 2. TimeZones/TimeZones/Controllers/ViewController.swift
@@ -25,11 +25,44 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        loadData()
         
+        title = "Time Zones"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addFriend))
+    }
+    
+    // MARK: Helper Methods
+    
+    func loadData() {
+        let defaults = UserDefaults.standard
         
+        guard let savedData = defaults.data(forKey: "Friends") else {
+            fatalError("Couldn't find the data.")
+        }
+        
+        let decoder = JSONDecoder()
+        
+        guard let savedFriends = try? decoder.decode([Friend].self, from: savedData) else {
+            return
+        }
+        
+        friends = savedFriends
+    }
+    
+    func saveData() {
+        let defaults = UserDefaults.standard
+        let encoder = JSONEncoder()
+        
+        guard let savedData = try? encoder.encode(friends) else {
+            fatalError("Unable to encode friends data.")
+        }
+        
+        defaults.set(savedData, forKey: "Friends")
     }
 
-
+    func addFriend() {
+        // TODO: Add method for bar button item
+    }
 }
 
 // MARK: - UIViewController: UITableViewDelegate, UITableViewDataSource
@@ -48,6 +81,9 @@ extension UIViewController: UITableViewDelegate, UITableViewDataSource {
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let friend = friends[indexPath.row]
+        cell.textLabel?.text = "\(friend.name)"
+        cell.detailTextLabel?.text = "\(friend.timeZome.identifier)"
         return cell
     }
     
