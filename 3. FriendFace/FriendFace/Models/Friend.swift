c33c3fff84dc2f3e9598a8e1598e69809d0cb126
Neil Hiddink <nhiddink2@gmail.com>
2019-01-27 13:49:00 -0500

    refactor: (FriendFace) Abstract out code from ViewController

--- 3. FriendFace/FriendFace/Models/Friend.swift
+++ 3. FriendFace/FriendFace/Models/Friend.swift
@@ -20,3 +20,17 @@ struct Friend: Codable {
     var tags: [String]
     var friends: [Connection]
 }
+
+extension Array where Element == Friend {
+    func matching(_ text: String?) -> [Friend] {
+        if let text = text, text.count > 0 {
+            return self.filter {
+                $0.name.contains(text)
+                || $0.company.contains(text)
+                || $0.address.contains(text)
+            }
+        } else {
+            return self
+        }
+    }
+}
