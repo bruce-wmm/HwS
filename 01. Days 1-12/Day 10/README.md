c7c712ed5c50a971f648235ebfab7a761a99f093
Neil Hiddink <nhiddink2@gmail.com>
2019-02-11 16:10:04 -0500

    feat: Complete Day 9 Tests

--- 01. Days 1-12/Day 10/README.md
+++ 01. Days 1-12/Day 10/README.md
@@ -2,6 +2,12 @@
 
 ## Day 10 Notes
 
+[Swift Docs - Structures and Classes](https://docs.swift.org/swift-book/LanguageGuide/ClassesAndStructures.html)
+
+```
+Structures and classes are general-purpose, flexible constructs that become the building blocks of your program’s code.
+```
+
 <p align="center"><img src="" width="500"></p>
 
 ### 
--- 01. Days 1-12/Day 9/Day9.playground/Contents.swift
+++ 01. Days 1-12/Day 9/Day9.playground/Contents.swift
@@ -1,14 +1,108 @@
 import UIKit
 
-// Structs - Part 1
+// Structs - Part 2
 
-struct Person {
+// Initializers
+
+struct Starship {
+    var name: String
+    var maxWarp: Double
+    init(starshipName: String) {
+        self.name = starshipName
+//        self.maxWarp = 0.0
+    }
+}
+let voyager = Starship(starshipName: "Voyager")
+
+// Static Properties and Methods
+
+struct Amplifier {
+    static let maximumVolume = 11
+    var currentVolume: Int
+}
+
+struct Student {
+    static var classSize = 0
     var name: String
     
-    mutating func makeAnonymous() {
-        name = "Anonymous"
+    init(name: String) {
+        self.name = name
+        Student.classSize += 1
     }
 }
 
-var person = Person(name: "Ed")
-person.makeAnonymous()
+let ed = Student(name: "Ed")
+let taylor = Student(name: "Taylor")
+
+struct Question {
+    static let answer = 42
+    var questionText = "Unknown"
+    init(questionText: String, answer: String) {
+        self.questionText = questionText
+        self.answer = answer
+    }
+}
+
+struct Person {
+    static var population = 0
+    var name: String
+    init(personName: String) {
+        name = personName
+        population += 1
+    }
+}
+
+struct Raffle {
+    var ticketsUsed = 0
+    var name: String
+    var tickets: Int
+    init(name: String, tickets: Int) {
+        self.name = name
+        self.tickets = tickets
+        Raffle.ticketsUsed += tickets
+    }
+}
+
+struct Cat {
+    static let allCats = [Cat]()
+    init() {
+        Cat.allCats.append(self)
+    }
+    static func chorus() {
+        for _ in allCats {
+            print("Meow!")
+        }
+    }
+}
+
+// Access Control
+
+struct SecretAgent {
+    private var actualName: String
+    public var codeName: String
+    init(name: String, codeName: String) {
+        self.actualName = name
+        self.codeName = codeName
+    }
+}
+let bond = SecretAgent(name: "James Bond", codeName: 007)
+
+struct Doctor {
+    var name: String
+    var location: String
+    static var currentPatient = "No one"
+}
+let drJones = Doctor(name: "Esther Jones", location: "Bristol")
+
+struct FacebookUser {
+    private var privatePosts: [String]
+//    public var publicPosts: [String]
+    var publicPosts: [String]
+}
+let user = FacebookUser()
+
+struct Contributor {
+    private var name = "Anonymous"
+    var age: Int
+}
+let paul = Contributor(age: 10)
