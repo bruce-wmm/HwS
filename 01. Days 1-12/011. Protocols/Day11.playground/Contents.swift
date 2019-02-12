6baaef2f97609425ba0e3489ddbd3283f6c2d43d
Neil Hiddink <nhiddink2@gmail.com>
2019-02-12 02:15:33 -0500

    feat: Complete Day 11 Tests

--- 01. Days 1-12/011. Protocols/Day11.playground/Contents.swift
+++ 01. Days 1-12/011. Protocols/Day11.playground/Contents.swift
@@ -1,3 +1,95 @@
 import UIKit
 
 // Protocols
+
+protocol Swimmable {
+    var depth { get }
+}
+
+// Protocol Inheritance
+
+protocol Scoreable {
+    func add
+}
+protocol Winnable {
+    func makeWinningSpeech
+}
+protocol Competitor: Scoreable, Winnable { }
+
+protocol Buyable {
+    var cost: Int
+}
+protocol Sellable {
+    func findBuyers() -> [String]
+}
+protocol FineArt: Buyable, Sellable { }
+
+protocol Readable {
+    func read() -> String
+}
+protocol Writeable {
+    func write(string: String)
+}
+protocol NetworkSocket: Readable, Writeable
+
+// Extensions
+
+extension String {
+    func append(_ other: String) {
+        self += other
+    }
+}
+
+extension Int {
+    times(_ action: () -> Void) {
+    for _ in 0..<self {
+    action()
+    }
+    }
+}
+
+// Protocol Extensions
+
+protocol SmartPhone {
+    func makeCall(to name: String)
+}
+extension SmartPhone {
+    func makeCall(to name: String) {
+        print("Dialling \(name)...")
+    }
+}
+
+// Summary - Protocols and Extensions
+
+// In this example, the mutating keyword is required even if you're just calling a class because age is an Int, which is a Struct and Struct's self property is immutable by default
+protocol HasAge {
+    var age: Int { get set }
+    mutating func celebrateBirthday()
+}
+
+class Man: HasAge {
+    var age = 50
+    
+    func celebrateBirthday() {
+        age += 1
+    }
+}
+
+struct Dog: HasAge {
+    var age = 1
+    
+    func celebrateBirthday() {
+        age *= 7
+    }
+}
+
+// Here the mutating isn't doing anything, so it works to
+// add mutating or remove it from in front of the extension's implementation of refillAir()
+protocol Inflatable {
+    mutating func refillAir()
+}
+extension Inflatable {
+    func refillAir() {
+        print("Refilling the air.")
+    }
+}
--- 01. Days 1-12/011. Protocols/README.md
+++ 01. Days 1-12/011. Protocols/README.md
@@ -2,6 +2,10 @@
 
 ## Day 11 Notes
 
+[Summary - Protocols and Extensions](https://youtu.be/q1UkvM2V07E)
+
+[Swift Language Guide - Protocols](https://docs.swift.org/swift-book/LanguageGuide/Protocols.html)
+
 <p align="center"><img src="" width="500"></p>
 
 ### 
@@ -12,4 +16,5 @@
 
 ### 💡 New Concepts:
 
--
+- Classes cannot have multiple inheritance, but Protocols can.
+- If you define a protocol instance method requirement that is intended to mutate instances of any type that adopts the protocol, mark the method with the `mutating` keyword as part of the protocol’s definition. This enables structures and enumerations to adopt the protocol and satisfy that method requirement. Remember that you don't need to add `mutating` when writing an implementation of that method for a class.
