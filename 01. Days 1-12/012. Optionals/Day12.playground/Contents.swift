be02c6f3a83fd8d7d5f5904d352ea52c2a4a11cf
Neil Hiddink <nhiddink2@gmail.com>
2019-02-15 16:03:20 -0500

    feat: Add Last 3 Tests from Day 12

--- 01. Days 1-12/012. Optionals/Day12.playground/Contents.swift
+++ 01. Days 1-12/012. Optionals/Day12.playground/Contents.swift
@@ -66,3 +66,48 @@ let firstItem = shoppingList.first?.appending(" are on my shopping list")
 
 let names = ["Taylor", "Paul", "Adele"]
 let lengthOfLast = names.last?.count?
+
+// Typecasting
+
+class Glasses {
+    var isShortSighted = true
+}
+struct Sunglasses: Glasses { }
+let shades = Sunglasses()
+if let glasses = shades as? Glasses {
+    if glasses.isShortSighted {
+        print("These sunglasses are for shortsighted people.")
+    } else {
+        print("These sunglasses are for longsighted people.")
+    }
+}
+
+let distances = [23, 28, 22]
+if let stringDistances = distances as [Int] {
+    for distance in stringDistances {
+        print("The distance was \(distance).")
+    }
+}
+
+class Sport { }
+class Swimming: Sport {
+    var distance = 100
+}
+let swimming = Sport()
+if let sport as? Swimming {
+    print("The distance is \(sport.distance).")
+}
+
+// Summary - Optionals
+
+let names = ["John", "Paul", "George", "Ringo"]
+let upperRingo = names.last.uppercased()
+
+let birthYear: Int? = nil
+let year = birthYear ?? "Unknown"
+
+func brewBeer(to strength: Double?) {
+    guard let strength = strength else { return }
+    print("Let's brew some beer!")
+}
+brewBeer(to: 5.5)
